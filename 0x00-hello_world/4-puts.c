1640a9038162700c26a5b31a8968aa75b939f8a3
mohamed <mksale13@hotmail.com>
2022-12-12 07:59:08 -0800

    task 401

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - main block
  * return: 0
- */
+*/
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 
 	return (0);
 
