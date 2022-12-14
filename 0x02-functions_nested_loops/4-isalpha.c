dbac214d6d59c593ecdcdd8e8eb44ab71fac877b
mohamed <mksale13@hotmail.com>
2022-12-14 14:36:47 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <main.h>
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: The character to be checked*Return: 1 alphabticcharacteror 0 anythinge.
+ * isalpha : int argument.
+ * main - print a
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || ((c <= 97 && c <= 122))
+			return (1);
+			return (0);
+			}
+			int main(void)
+			{
+			char 'a';
+			printf("%d\n", our_isalpha('a' '-1'));
+			printf("%d\n", our_isalpha('a'));
+			return (0);
+			}
