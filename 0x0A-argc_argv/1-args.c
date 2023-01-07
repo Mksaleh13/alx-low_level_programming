1c3287ddc30f45eee53f5b041319942581d154ef
mohamed <mksale13@hotmail.com>
2023-01-06 22:48:31 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function to print name of file
+ * @argc: is count of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
